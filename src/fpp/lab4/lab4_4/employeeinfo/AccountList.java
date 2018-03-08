adcf920712324152262c84012110774840bd32bf
segun <segxy4show2000@gmail.com>
2018-03-08 07:18:30 -0600

    Employee class modified for get list options

--- src/fpp/lab4/lab4_4/employeeinfo/AccountList.java
+++ src/fpp/lab4/lab4_4/employeeinfo/AccountList.java
@@ -1,9 +1,72 @@
 package fpp.lab4.lab4_4.employeeinfo;
 
+
 public class AccountList {
-   public static fpp.lab4.lab4_4.AccountList accountList;
-   static {
-       accountList = new fpp.lab4.lab4_4.AccountList();
-   }
+
+    private Account[] accounts = new Account[50];
+    private int size = 0;
+    public AccountList(){
+    }
+
+    public void add(Account account){
+        if(size == accounts.length){
+            resize();
+        }
+        accounts[size++] = account;
+    }
+
+    public boolean find(Account account){
+        for(int i = 0; i < size; i++){
+            if(accounts[i].equals(account));
+            return true;
+        }
+        return false;
+    }
+
+    public Account get(int index){
+        return accounts[index];
+    }
+
+    public boolean remove(Account account){
+        boolean found = false;
+        for(int i = 0; i < size; i++){
+            if(accounts[i].equals(account)) {
+                found = true;
+                for(int j = i; j < size - 1; j++){
+                    accounts[j] = accounts[j+1];
+                }
+                accounts[size-1] = null;
+                size--;
+                break;
+            }
+        }
+        return found;
+    }
+
+    @Override
+    public String toString(){
+        String output = "[";
+        for(int i = 0; i < size; i++){
+            output +=accounts[i];
+            if(accounts[i+1] != null)
+                output+=", ";
+        }
+        output += "]";
+        return output;
+    }
+
+    public int size(){
+        return size;
+    }
+
+    private void resize(){
+        System.out.println("Resizing...");
+        Account[] temp = new Account[accounts.length * 2];
+        System.arraycopy(accounts, 0, temp, 0, accounts.length);
+        accounts = temp;
+    }
+
 
 }
+
+
--- src/fpp/lab4/lab4_4/employeeinfo/Main.java
+++ src/fpp/lab4/lab4_4/employeeinfo/Main.java
@@ -5,12 +5,23 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        Employee[] emps = new Employee[3];
+        emps[0] = new Employee("Jim Daley", 2000, 9, 4);
+        emps[1] = new Employee("Bob Reuben", 1998, 1, 5);
+        emps[2] = new Employee("Susan Randolph", 1997, 2, 13);
+        emps[0].createNewChecking(10500);
+        emps[0].createNewSavings(1000);
+        emps[0].createNewRetirement(9300);
+        emps[1].createNewChecking(34000);
+        emps[1].createNewSavings(27000);
+        emps[1].createNewRetirement(37000);
+        emps[2].createNewChecking(10038);
+        emps[2].createNewSavings(12600);
+        emps[2].createNewRetirement(9000);
+        displayMenu();
         Scanner sc = new Scanner(System.in);
-        System.out.print("A. See a report of all account? (y/n) ");
-        System.out.print("B. Make a deposity ");
-        System.out.print("C. See a report of all account? (y/n) ");
         String answer = sc.next();
-        if (answer.equalsIgnoreCase("y")) {
+        if (answer.equalsIgnoreCase("A")) {
             for (Employee employee : emps) {
                 System.out.println("ACCOUNT INFO FOR " + employee.toString());
                 String info = employee.getFormattedAcctInfo();
@@ -20,6 +31,21 @@ public class Main {
         } else {
             //do nothing..the application ends here
         }
+    }
+
+    public static void displayMenu(){
+        System.out.print("A. See a report of all account? (y/n) ");
+        System.out.print("B. Make a deposit ");
+        System.out.print("C. See a report of all account? (y/n) ");
+        System.out.print("Make a selection (A/B/C)");
+    }
+
+    public static void displayOPtionToSeeReport(Employee[] employees){
+        int index = 0;
+        for(Employee employee : employees){
+            System.out.println(index++ + ". " + employee.toString());
+        }
+
     }
 
 
